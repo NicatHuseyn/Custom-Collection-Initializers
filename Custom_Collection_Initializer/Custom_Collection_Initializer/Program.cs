a3aeef15712a9822397daf6038a076a548ca06e7
NicatHuseyn <nicat5075@gmail.com>
2024-12-11 03:37:40 -0800

    custom collection initializer

---
+++ Custom_Collection_Initializer/Custom_Collection_Initializer/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+using System.Collections;
+
+
+MyClass ints = new MyClass()
+{
+    {5,"salam" },
+    {3,"sagol" },
+};
+
+class MyClass : IEnumerable<int>
+{
+    List<int> numbers;
+
+    public void Add(int i, string j)=>numbers.Add(i);
+    public IEnumerator<int> GetEnumerator()=>numbers.GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator()=> numbers.GetEnumerator();
+}
+
+
+
